92b92e644b01ffa0f3c104b0587e69a19caa7c36
Vasilii <jud3dx@gmail.com>
2021-03-11 16:05:53 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// alert('hello');
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = prompt('You age 18?', '18');
+// console.log(typeof answer);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`hallo, ${user}`);
+
+// let incr = 10,
+//   decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+const isChecked = true,
+  isClose = true;
+
+console.log(isChecked || isClose);
+
+console.log(2 + 2 * 2 === 8);
